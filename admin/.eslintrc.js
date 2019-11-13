338609db187559ceb0d539e5b579d4c3a8eb051c
dongwudi <550293874@qq.com>
2019-11-13 23:03:33 +0800

    eslint autosave format

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
@@ -22,4 +23,4 @@ module.exports = {
       }
     }
   ]
-}
+};
--- admin/src/main.js
+++ admin/src/main.js
@@ -2,12 +2,41 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button, Select } from 'element-ui'
+import {
+  Button,
+  Select,
+  Container,
+  Aside,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Header,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Main,
+  Table,
+  TableColumn
+} from 'element-ui'
 
 Vue.config.productionTip = false
 
 Vue.use(Button)
 Vue.use(Select)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Header)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Main)
+Vue.use(Table)
+Vue.use(TableColumn)
 
 new Vue({
   router,
