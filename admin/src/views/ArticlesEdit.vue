f769c7b536427df81b0e3b7ece27dcf2c4f88865
dongwudi <550293874@qq.com>
2019-11-16 23:23:42 +0800

    articleEdit

--- admin/src/views/ArticlesEdit.vue
+++ admin/src/views/ArticlesEdit.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="about">
     <h2>{{ id ? '编辑' : '新建' }}文章</h2>
-    <el-form
-      @submit.native.prevent="save"
-      label-position="right"
-      label-width="80px"
-    >
+    <el-form @submit.native.prevent="save" label-position="right" label-width="80px">
       <el-form-item label="所属分类">
         <el-select v-model="model.categories" multiple placeholder="请选择">
-          <el-option
-            v-for="item in categories"
-            :key="item._id"
-            :label="item.name"
-            :value="item._id"
-          ></el-option>
+          <el-option v-for="item in categories" :key="item._id" :label="item.name" :value="item._id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="标题">
         <el-input v-model.trim="model.title"></el-input>
       </el-form-item>
       <el-form-item label="详情">
-        <vue-editor v-model.trim="model.body"></vue-editor>
+        <vue-editor v-model.trim="model.body" useCustomImageHandler @image-added="handleImageAdded"></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -44,6 +35,13 @@ export default {
     }
   },
   methods: {
+    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
+      const formData = new FormData()
+      formData.append('file', file)
+      let res = await this.$http.post('upload', formData)
+      Editor.insertEmbed(cursorLocation, 'image', res.data.url)
+      resetUploader()
+    },
     async save() {
       let res
       // 未输入标题
