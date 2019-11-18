196b346d8fd38dee70a03988b4f8352faad26d1a
dongwudi <550293874@qq.com>
2019-11-18 23:18:45 +0800

    style

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="about">
+    <h1>This is an home page</h1>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-1 px-1 d-flex ai-center">
+      <img src="../assets/logo.jpg" alt="" height="35">
+      <div class="pl-2 d-flex flex-column jc-center ai-left">
+        <div class="text-white">英雄联盟</div>
+        <div class="text-grey-1 fs-xs">League of Legends</div>
+      </div>
+    </div>
+    <div class="nav bg-primary py-2">
+      <div class="nav d-flex text-white jc-around pb-1">
+        <div class="nav-item active">
+          <router-link class="nav-link" tag="div" to="/">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.nav{
+  .nav-item{
+    border-bottom: none;
+    padding-bottom: 0.2rem;
+    transition: all .2s;
+  }
+  .active{
+    border-bottom: 2px solid #fff;
+  }
+}
+</style>
